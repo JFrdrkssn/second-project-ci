ac913dee5b66321361c7ff827b5b42574c22799d
JFrdrkssn <johanf9401@gmail.com>
2022-02-08 16:13:58 +0000

    Add js code to draw up game grid and enemies

---
+++ js/script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() { });
+
+
+/* The code basis for this game comes from
+    this tutorial https://www.youtube.com/watch?v=3Nz4Yp7Y_uA
+    Source code from Ania Kubow https://github.com/kubowania/space-invaders
+*/
+
+const grid = document.querySelector('.game-grid')
+
+/**
+ * This for loop creates the grid layout the game operates on
+ */
+for (let i = 0; i < 368; i++) {
+    const square = document.createElement('div')
+    grid.appendChild(square)
+}
+
+/**
+ * This variable provides the basis to iterate through the grid and and enemies
+ */
+const squares = Array.from(document.querySelectorAll('.game-grid div'))
+
+
+/**
+ * This is the number of alien enemies
+ */
+const aliens = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38
+]
+
+/**
+ * This code adds styling to the aliens array (enemies) above
+ */
+function draw() {
+    for (let i = 0; i < aliens.length; i++) {
+        squares[aliens[i]].classList.add('alien-enemies')
+    }
+}
+
+draw()
