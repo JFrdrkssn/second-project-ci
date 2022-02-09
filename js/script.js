619af56627d7ff7a73c7e7742bca592a9f450636
JFrdrkssn <johanf9401@gmail.com>
2022-02-09 19:53:15 +0000

    Update number and position of aliens in game grid

--- js/script.js
+++ js/script.js
@@ -37,11 +37,12 @@ const squares = Array.from(document.querySelectorAll('.game-grid div'))
 
 
 /**
- * This is the number of alien enemies
+ * This is the number of alien enemies and their position in the grid
  */
 const aliens = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 
+    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
 ]
 
 /**
