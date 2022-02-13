90370cb0ec961f0b6836abc0de76fe06728da7be
JFrdrkssn <johanf9401@gmail.com>
2022-02-13 11:13:43 +0000

    Final tidy up

--- js/script.js
+++ js/script.js
@@ -15,20 +15,6 @@ function menu() {
   });
 }
 
-/**  The code basis for this game comes from
- *   this tutorial https://www.youtube.com/watch?v=3Nz4Yp7Y_uA
- *   Source code from Ania Kubow https://github.com/kubowania/space-invaders
- *   MIT Licence
- *   Copyright (c) 2020 Ania Kubow
- *
- *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- *   Translation: Ofcourse you can use this for you project! Just make sure to say where you got this from :)
-*/
-
-
 function startGame() {
   // This is the width of the grid in terms of squares
   let width = 15;
@@ -245,3 +231,16 @@ function startGame() {
 
   document.addEventListener("keydown", fire);
 }
+
+/**  The code basis for this game comes from
+ *   this tutorial https://www.youtube.com/watch?v=3Nz4Yp7Y_uA
+ *   Source code from Ania Kubow https://github.com/kubowania/space-invaders
+ *   MIT Licence
+ *   Copyright (c) 2020 Ania Kubow
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *   Translation: Ofcourse you can use this for you project! Just make sure to say where you got this from :)
+*/
